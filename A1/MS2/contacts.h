04418f405d91599b0824bd13d555b9e22a09b884
frankxiao008 <40275663+frankxiao008@users.noreply.github.com>
2018-07-05 14:45:01 -0400

    Update contacts.h

--- A1/MS2/contacts.h
+++ A1/MS2/contacts.h
@@ -10,11 +10,26 @@ Milestone:  2
 ---------------------------------------------- */
 
 // Structure type Name declaration
+
 struct Name {
     char firstName[31];
     char middleInitial[7];
     char lastName[36];
 };
+
+struct Address {
+    int streetNumber;
+    char street[41];
+    int apartmentNumber
+    char postalCode[8];
+    char city[41];
+}
+
+struct Numbers {
+    char cell[21];
+    char home[21];
+    char business[21];
+}
 
 // Structure type Address declaration
 // Place your code from Milestone #1 here...
